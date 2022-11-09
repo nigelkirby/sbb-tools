608acc5faa29fd97f334c99769a97285b908873a
Nigel Kirby <nigel.kirby@gmail.com>
2022-11-09 02:03:24 -0600

    organization

--- src/CharArt.js
+++
@@ -1,6 +0,0 @@
-export default function CharArt({ charId }) {
-  const name = charId.replace(',', '').replace("'", '')
-  return (
-    <img src={`${process.env.PUBLIC_URL}/cards/${name}.png`} alt={charId} />
-  )
-}
